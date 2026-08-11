56a14c0a94c7afa68536c2d2ba5f0a318e41f1e9
Ramsey-Javan <javanbosire@gmail.com>
2026-08-11 22:51:03 +0300

    Fixed Logo access permissions

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -22,3 +22,8 @@ AWS_ACCESS_KEY_ID = config('R2_ACCESS_KEY_ID', default='')
 AWS_SECRET_ACCESS_KEY = config('R2_SECRET_ACCESS_KEY', default='')
 AWS_STORAGE_BUCKET_NAME = config('R2_BUCKET_NAME', default='')
 AWS_S3_ENDPOINT_URL = config('R2_ENDPOINT_URL', default='')
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'auto'          # R2 uses 'auto' as its region
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_QUERYSTRING_AUTH = False         # produce clean public URLs, not expiring presigned ones
+AWS_DEFAULT_ACL = None               # R2 doesn't use S3 ACLs
